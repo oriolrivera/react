257bb498b4556d8d92669f34c5f98a62c6fb1691
sheolseol <siolsiol@hotmail.com>
2017-03-25 15:22:01 -0400

    implement server webpack

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,15 @@
-require('./moduo1.js');
-require('./modulo2.js');
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, browserHistory, IndexRoute  } from 'react-router'
+
+class Layout extends React.Component {
+    render(){
+        return(
+            <h1>Test react</h1>
+        );
+    }   
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,16 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
+    module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+           presets:["react","es2015","stage-0"],
+           plugins:["react-html-attrs","transform-class-properties","transform-decorators-legacy"],
+        }
+      },
+    ]
+}
 };
